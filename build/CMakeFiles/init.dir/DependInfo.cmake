24d599e784fe7362879e363c9b187c9e80a10325
danielgbwilson <daniel.wilson@maths.ox.ac.uk>
2016-05-09 12:09:42 +0100

    Add files via upload

---
+++ build/CMakeFiles/init.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wilsond/Desktop/aboria_init/Zero Flux/init.cpp" "/home/wilsond/Desktop/aboria_init/Zero Flux/build/CMakeFiles/init.dir/init.cpp.o"
+  "/home/wilsond/Desktop/aboria_init/Zero Flux/python.cpp" "/home/wilsond/Desktop/aboria_init/Zero Flux/build/CMakeFiles/init.dir/python.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "HAVE_VTK"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/vtk-5.8"
+  "/usr/include/python2.7"
+  "/usr/include/x86_64-linux-gnu/python2.7"
+  "../Aboria/src"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/init.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/init.dir/python.cpp.o"
+  "CMakeFiles/init.dir/init.cpp.o"
+  "init.pdb"
+  "init.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/init.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
